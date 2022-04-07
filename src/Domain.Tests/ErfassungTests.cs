c368f6eb815b292a9176b882d25ce9b7050a03cc
Alexander Groß <agross@therightstuff.de>
2022-04-07 18:31:41 +0200

    Basic recording of travel activities

---
+++ src/Domain.Tests/ErfassungTests.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace Domain.Tests;
+
+[TestFixture]
+public class ErfassungTests
+{
+  [Test]
+  public void Soll_Reisen_mit_Anfang_Ende_Zielort_und_Reisegrund_akzeptieren()
+  {
+    var anfang = DateTime.MinValue;
+    var ende = DateTime.MinValue;
+    var zielort = "egal";
+    var grund = "egal";
+
+    Erfasse(anfang, ende, zielort, grund);
+  }
+
+  [Test]
+  public void Soll_Reisen_ablehnen_deren_Ende_vor_dem_Beginn_liegt()
+  {
+    var anfang = DateTime.MaxValue;
+    var ende = DateTime.MinValue;
+    var zielort = "egal";
+    var grund = "egal";
+
+    FluentActions.Invoking(() => Erfasse(anfang, ende, zielort, grund))
+                 .Should()
+                 .Throw<EndeDerReiseMussNachReisebeginnLiegen>();
+  }
+
+  [Test]
+  public void Soll_zu_einem_Zeitpunkt_nur_eine_Reise_akzeptieren()
+  {
+    var anfang = DateTime.MinValue;
+    var ende = DateTime.MaxValue;
+    var zielort = "egal";
+    var grund = "egal";
+
+    Erfasse(anfang, ende, zielort, grund);
+
+    FluentActions.Invoking(() => Erfasse(anfang, ende, zielort, grund))
+                 .Should()
+                 .Throw<ZuEinemZeitpunktDarfNurEineReiseErfasstWerden>();
+  }
+
+  List<Reise> _buchhaltung = new();
+
+  void Erfasse(DateTime anfang, DateTime ende, string zielort, string grund)
+  {
+    if (ende < anfang)
+    {
+      throw new EndeDerReiseMussNachReisebeginnLiegen();
+    }
+
+    if (_buchhaltung.Any(reise => reise.Anfang <= anfang && reise.Ende >= ende))
+    {
+      throw new ZuEinemZeitpunktDarfNurEineReiseErfasstWerden();
+    }
+
+    _buchhaltung.Add(new Reise(anfang, ende));
+  }
+}
+
+record Reise(DateTime Anfang, DateTime Ende);
+
+public class EndeDerReiseMussNachReisebeginnLiegen : Exception
+{
+}
+
+public class ZuEinemZeitpunktDarfNurEineReiseErfasstWerden : Exception
+{
+}
